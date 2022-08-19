45964d232c8471cf77a2ea84c5a3a560726e7c02
Stephan Wels <stephan.wels@wels-holding.com>
2022-08-19 15:04:45 +0000

    fix module

--- binary_search_test.go
+++ binary_search_test.go
@@ -14,6 +14,6 @@ func TestIndexOf(t *testing.T) {
 
 var _ = Describe("Binary Search", func() {
 	It("should find an element", func() {
-		Expect(IndexOf([]int{1,2,3,4}, 3)).To(Equal(2))
+		Expect(IndexOf([]int{1, 2, 3, 4}, 3)).To(Equal(2))
 	})
 })
